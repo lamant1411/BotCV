4f716899c4f31d73ef33b537c6dae094bacfbb19
nguyen the lam <lamx8qb@gmail.com>
2026-04-03 16:17:41 +0700

    .

--- frontend/src/services/companyService.js
+++
@@ -1,9 +0,0 @@
-// src/services/companyService.js
-export const companyService = {
-  getCompanyProfile: async () => {
-    // API call hoặc mock data
-  },
-  updateCompanyProfile: async (data) => {
-    // API call
-  }
-};
